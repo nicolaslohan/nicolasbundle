5b6bb7dfe9aea61eaf9211ac46f197433780d047
nicolas <nicolauraujo@gmail.com>
2022-07-31 18:15:24 -0300

    responsive fix

--- animation.js
+++ animation.js
@@ -1,7 +1,35 @@
+
 const observer = new IntersectionObserver(entries => {
     console.log(entries);
-    entries[0].target.classList.add('init-hidden-off');
+    Array.from(entries).forEach((entry) => {
+        if(entry.intersectionRatio >= 1){
+            entry.target.classList.add('init-hidden-off')
+        }
+    })
+}, {
+    threshold: [0, .5, 1]
 })
-Array.from(document.querySelector('.init-hidden')).forEach(element => 
-    observer.observe(document.querySelector(element))
-    )
+
+Array.from(document.querySelectorAll('.init-hidden')).forEach((element) => {
+    observer.observe(element)
+})
+
+function bounce_click(){
+    Array.from(document.querySelectorAll('#button')).forEach((element)=>{
+        element.addEventListener('click', button =>{
+            button.target.classList.add('bounce')
+            button.offsetHeight
+        })
+    })
+}
+
+function animation(button){
+    button.classList.add('bounce')
+    reset_animation(button)
+}
+
+function reset_animation(button) {
+    button.style.animation = 'none';
+    button.offsetHeight;
+    button.style.animation = null; 
+  }
