82cbb0d8b804bd9620672f4fb675ecfeefcb5f05
nicolas <nicolauraujo@gmail.com>
2022-08-03 22:17:05 -0300

    tools page updated

--- animation.js
+++ animation.js
@@ -1,29 +1,13 @@
-
-const observer = new IntersectionObserver(entries => {
-    console.log(entries);
-    Array.from(entries).forEach((entry) => {
-        if(entry.intersectionRatio >= 1){
-            entry.target.classList.add('init-hidden-off')
-        }
-    })
-}, {
-    threshold: [0, .5, 1]
-})
-
-Array.from(document.querySelectorAll('.init-hidden')).forEach((element) => {
-    observer.observe(element)
-})
-
-function bounce_click(){
-    Array.from(document.querySelectorAll('#button')).forEach((element)=>{
-        element.addEventListener('click', button =>{
+function bounce_click() {
+    Array.from(document.querySelectorAll('#button')).forEach((element) => {
+        element.addEventListener('click', button => {
             button.target.classList.add('bounce')
             button.offsetHeight
         })
     })
 }
 
-function animation(button){
+function animation(button) {
     button.classList.add('bounce')
     reset_animation(button)
 }
@@ -31,5 +15,37 @@ function animation(button){
 function reset_animation(button) {
     button.style.animation = 'none';
     button.offsetHeight;
-    button.style.animation = null; 
-  }
+    button.style.animation = null;
+}
+
+const hidden = document.querySelectorAll('section');
+const fadeOptions = {
+    root: null,
+    rootMargin: "0px"
+  };
+
+  hidden.forEach(element => {
+    element.classList.add('loadin');
+  })
+
+
+
+const fadeObserver = new IntersectionObserver(function (entries, onScreen) {
+    Array.from(entries).forEach(entry => {
+
+        console.log(entry);
+        if(entry.isIntersecting){
+            entry.target.classList.add('loaded');
+        }
+        else{
+            entry.target.classList.remove('loaded');
+        }
+    })
+}, fadeOptions);
+
+
+hidden.forEach(element => {
+    fadeObserver.observe(element)
+})
+
+console.log(hidden)
